cec70db20d8f424d48377e63ba04d58b62b388c5
momokokong <momokokong@gmail.com>
2024-06-09 00:03:07 -0800

    Add a .js file with for loop to print topics

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let x = 0; x < topics.length; x ++){
+    console.log(topics[x]);
+}
+
+/*
+if (topic === "HTML") {
+    console.log("Let's study HTML");
+} else if (topic === "CSS") {
+    console.log("Let's study CSS");
+} else if (topic === "GIT") {
+    console.log("Let's study GIT");
+} else if (topic === "JavaScript") {
+    console.log("Let's study JaveScript");
+} else {
+    console.log("Let's study again?");
+}
+*/
